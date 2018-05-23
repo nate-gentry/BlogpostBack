f1230f7b80da15d5b56dec6089579dcee0b6c1a6
Nikita Zamiatin <4406015@gmail.com>
2018-05-23 10:10:52 -0400

    create 4 folders

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3007;
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended :true}));
 
 app.get('/', (request , response)=>{
   response.send('ajax-blog-works');
